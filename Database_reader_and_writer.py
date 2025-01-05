e4bae341c6365b8cc8b36ec66173281ec388f1f0
Enjoy-Maity <enjoymaity@gmail.com>
2025-01-05 22:21:55 +0530

    Created new methods for database table creation

--- Database_reader_and_writer.py
+++ Database_reader_and_writer.py
@@ -1,4 +1,5 @@
 import sqlite3
+from tkinter import messagebox
 
 
 class DataBase_Handler:
@@ -8,19 +9,41 @@ class DataBase_Handler:
         self.table_name = project_name
         self.import_table = "Import_Table"
         self.packages_table = "Packages_Table"
+        self.modules_table = "Modules_Table"
+        self.methods_table = "Methods_Table"
+        self.classes_table = "Classes_Table"
+        self.variables_table = "Variables_Table"
+        self.create_packages_table()
+        self.create_modules_table()
+        self.create_import_table()
 
     def db_checker(self):
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='?';", (self.table_name,))
         return self.cursor.fetchone() is not None
 
-    def create_table(self):
-        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ("
+    def create_packages_table(self):
+        try:
+            self.start_transaction()
+            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.packages_table} ("
+                                f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                f"package_name TEXT, "
+                                "package_path TEXT, "
+                                "parent_package TEXT,"
+                                "obfuscated_name TEXT, );")
+            self.commit_transaction()
+        except sqlite3.Error as e:
+            messagebox.showerror(f"Error ({e.__class__.__name__})", f"Error while creating packages table: {e}")
+            self.rollback_transaction()
+
+    def create_modules_table(self):
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.modules_table} ("
                             f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                            f"package_name TEXT, "
-                            f"obfuscated_package_name TEXT, "
-                            f"obfuscation_status TEXT, "
-                            f"obfuscation_time TEXT, "
-                            f"error_message TEXT);")
+                            f"module_name TEXT, "
+                            "module_path TEXT, "
+                            "parent_package TEXT, "
+                            "package_id INTEGER, "
+                            "obfuscated_module_name TEXT, "
+                            "FOREIGN KEY (package_id) REFERENCES {self.packages_table}(id))")
 
     def create_import_table(self):
         self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.import_table} ("
@@ -29,8 +52,51 @@ class DataBase_Handler:
                             "method_name TEXT, "
                             "class_name TEXT, ")
 
+    def create_methods_table(self):
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.methods_table} ("
+                            f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            f"method_name TEXT, "
+                            f"module_name TEXT, "
+                            "parent_package TEXT, "
+                            "package_name TEXT, "
+                            "obfuscated_method_name TEXT, "
+                            "FOREIGN KEY (package_name) REFERENCES {self.packages_table}(package_name),"
+                            "FOREIGN KEY (module_name) REFERENCES {self.modules_table}(module_name))")
+
+    def create_classes_table(self):
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.classes_table} ("
+                            f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            f"class_name TEXT, "
+                            f"module_name TEXT, "
+                            "parent_package TEXT, "
+                            "package_name TEXT, "
+                            "obfuscated_class_name TEXT, "
+                            "FOREIGN KEY (package_name) REFERENCES {self.packages_table}(package_name),"
+                            "FOREIGN KEY (module_name) REFERENCES {self.modules_table}(module_name))")
+
+    def create_variables_table(self):
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.variables_table} ("
+                            f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            f"variable_name TEXT, "
+                            f"class_name TEXT, "
+                            "module_name TEXT, "
+                            "method_name TEXT, "
+                            "variable_type TEXT, "
+                            "parent_package TEXT, "
+                            "package_name TEXT, "
+                            "obfuscated_variable_name TEXT, "
+                            "FOREIGN KEY (package_name) REFERENCES {self.packages_table}(package_name),"
+                            "FOREIGN KEY (module_name) REFERENCES {self.modules_table}(module_name))")
+
     def db_writer(self, query):
-        self.cursor.execute(query)
+        try:
+            self.start_transaction()
+            self.cursor.execute(query)
+            self.commit_transaction()
+
+        except sqlite3.Error as e:
+            messagebox.showerror(f"Error ({e.__class__.__name__})", f"Error while writing to database: {e}")
+            self.rollback_transaction()
 
     def db_reader(self, query):
         self.cursor.execute(query)
